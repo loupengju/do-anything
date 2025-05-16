10fc5ec7e651c9a145ef755f06225e32b805cf56
loupengju <34649676@qq.com>
2025-05-16 14:52:21 +0800

    feat: 更新Next.js配置以支持额外的服务器外部包
    - 在next.config.ts中添加serverExternalPackages选项，包含@resvg/resvg-js、svgo和svg-sprite，以优化服务器端构建过程。

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  serverExternalPackages: ["@resvg/resvg-js", "svgo", "svg-sprite"], // 添加 svgo 和/或 svg-sprite
   /* config options here */
   webpack: (config, { isServer }) => {
     // Ensure this rule only applies to server-side builds
